5daa1c69dc728508355c69ba350c9631040b5bf9
Ashok <pavanashok5584@gmail.com>
2025-02-03 22:30:59 +0530

    terraform

---
+++ count/variables.tf
@@ -0,0 +1,19 @@
+variable "instances" {
+  default = ["mysql", "backend", "frontend"]
+}
+
+variable "zone_id" {
+  default = "Z099819629ZVWKL9S8414"
+}
+
+variable "domain_name" {
+  default = "dpak.online"
+}
+
+variable "common_tags" {
+  type = map 
+  default = {
+    Project = "expense"
+    Environment = "dev"
+  }
+}
