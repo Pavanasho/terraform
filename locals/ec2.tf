f0e214ef3983061307a9658686490602d50bcbea
Ashok <pavanashok5584@gmail.com>
2025-02-05 23:24:17 +0530

    terraform

---
+++ locals/ec2.tf
@@ -0,0 +1,32 @@
+resource "aws_instance" "this" {
+  ami                    = local.ami_id # This is our devops-practice AMI ID
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  #instance_type          = "t3.micro"
+  instance_type = local.instance_type
+  tags = {
+    Name    = local.name
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
---
+++ locals/variables.tf
@@ -0,0 +1,16 @@
+variable "instance_type" {
+  type = string
+  default = "t3.micro"
+}
+
+variable "project" {
+  default = "expense"
+}
+
+variable "component" {
+  default = "backend"
+}
+
+variable "environment" {
+  default = "dev"
+}
